8c7a34bbabcb51ee9f9773aa343b925b66bedf72
Kahlil <kahlil12@hotmail.com>
2024-12-01 18:06:38 -0500

    Adding ref to player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useState, useRef } from "react";
+
+
 export default function Player() {
+
+  const [enteredplayerName, setPlayerName] = useState(null);
+ 
+
+  //allows you to map to an element using ref property. Input below
+  const playerName = useRef();
+
+  
+
+  function handleSubmit(){
+   setPlayerName(playerName.current.value)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {
+        //If the value being checked is the same as will be displayed. If varaible is true use it otherwise use false value
+      }
+      <h2>Welcome {enteredplayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
