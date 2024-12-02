bd1d88efc12f7414947ea40f3fa86bcf8e2ec7e3
Kahlil <kahlil12@hotmail.com>
2024-12-02 14:39:34 -0500

    Updating timer challenge with interval and optimizing flow

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,26 +6,38 @@ import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({title, targetTime}) {
 
-    const [isTimeExpired, setIsTimeExpired] = useState(false);
-    const [isTimeStarted, setIsTimeStarted] = useState(false);
+    // const [isTimeExpired, setIsTimeExpired] = useState(false);
+    // const [isTimeStarted, setIsTimeStarted] = useState(false);
 
     const timer = useRef();
     const showDialog = useRef();
 
+    const [timeRemaing, setTimeRemaining] = useState(targetTime * 1000);
 
-    function handleStart(){
-        timer.current = setTimeout(() => {
-            setIsTimeExpired(true);
-            //Used to display modal when challenge is lost and function as expected with built-in backdrop
-            //uses function from result modal to decouple from time challenge component
-            showDialog.current.open();
-        }, targetTime * 1000);
+    const timerIsActive = timeRemaing > 0 && timeRemaing < (targetTime * 1000);
 
-        setIsTimeStarted(true);
+    //If we do not stop the timer in time and time runs out
+    if(timeRemaing <= 0 ){
+        clearInterval(timer.current)
+       setTimeRemaining(targetTime * 1000);
+       showDialog.current.open();
     }
 
+
+    function handleStart(){
+        timer.current = setInterval(() => {
+            //Used to display modal when challenge is lost and function as expected with built-in backdrop
+            //uses function from result modal to decouple from time challenge component
+            // showDialog.current.open();
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        }, 10);
+
+    }
+
+    //Manually stopping the timer
     function handleStop() {
-        clearTimeout(timer.current);
+        clearInterval(timer.current);
+        showDialog.current.open();
     }
 
     return(
@@ -37,12 +49,12 @@ export default function TimerChallenge({title, targetTime}) {
                     {targetTime} second{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={isTimeStarted ? handleStop : handleStart}>
-                        {isTimeStarted ? 'Stop' :'Start'} Challenge
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? 'Stop' :'Start'} Challenge
                     </button>
                 </p>
-                <p className={isTimeStarted ? 'active' : undefined}>
-                    {isTimeStarted ? 'Time is running' : 'Time is not running'}
+                <p className={timerIsActive ? 'active' : undefined}>
+                    {timerIsActive ? 'Time is running' : 'Time is not running'}
                 </p>
             </section>
         </>
